547ca375c858de81e4b6d78e067d6050b6a0116e
Mohammadamin-Ahmadi <mhmdamynahmdy123@gmail.com>
2024-11-27 12:26:36 +0330

    Add files via upload

---
+++ C#/WindowsForm/Codes project/WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,266 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.فایلهاToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.گولخوردیفایلنداریمToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.راهنماToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.دلتخوشهراهنماکجابودToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.حالاکهاصرارداریبیاToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.پشتیبانیToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.شمارهمیخوایتعارفنکنToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHelp = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHelpExit1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFileNo = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuHelpExit2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arabic Typesetting", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(164, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 55);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "سلام";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.فایلهاToolStripMenuItem,
+            this.راهنماToolStripMenuItem,
+            this.پشتیبانیToolStripMenuItem,
+            this.mnuHelp});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(424, 24);
+            this.menuStrip1.TabIndex = 8;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // فایلهاToolStripMenuItem
+            // 
+            this.فایلهاToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.گولخوردیفایلنداریمToolStripMenuItem});
+            this.فایلهاToolStripMenuItem.Name = "فایلهاToolStripMenuItem";
+            this.فایلهاToolStripMenuItem.Size = new System.Drawing.Size(54, 20);
+            this.فایلهاToolStripMenuItem.Text = "فایل ها";
+            // 
+            // گولخوردیفایلنداریمToolStripMenuItem
+            // 
+            this.گولخوردیفایلنداریمToolStripMenuItem.Name = "گولخوردیفایلنداریمToolStripMenuItem";
+            this.گولخوردیفایلنداریمToolStripMenuItem.Size = new System.Drawing.Size(186, 22);
+            this.گولخوردیفایلنداریمToolStripMenuItem.Text = "گول خوردی فایل نداریم";
+            // 
+            // راهنماToolStripMenuItem
+            // 
+            this.راهنماToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.دلتخوشهراهنماکجابودToolStripMenuItem});
+            this.راهنماToolStripMenuItem.Name = "راهنماToolStripMenuItem";
+            this.راهنماToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.راهنماToolStripMenuItem.Text = "راهنما";
+            // 
+            // دلتخوشهراهنماکجابودToolStripMenuItem
+            // 
+            this.دلتخوشهراهنماکجابودToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.حالاکهاصرارداریبیاToolStripMenuItem});
+            this.دلتخوشهراهنماکجابودToolStripMenuItem.Name = "دلتخوشهراهنماکجابودToolStripMenuItem";
+            this.دلتخوشهراهنماکجابودToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.دلتخوشهراهنماکجابودToolStripMenuItem.Text = "دلت خوشه راهنما کجا بود";
+            // 
+            // حالاکهاصرارداریبیاToolStripMenuItem
+            // 
+            this.حالاکهاصرارداریبیاToolStripMenuItem.Name = "حالاکهاصرارداریبیاToolStripMenuItem";
+            this.حالاکهاصرارداریبیاToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.حالاکهاصرارداریبیاToolStripMenuItem.Text = "حالا که اصرار داری بیا";
+            this.حالاکهاصرارداریبیاToolStripMenuItem.Click += new System.EventHandler(this.حالاکهاصرارداریبیاToolStripMenuItem_Click_1);
+            // 
+            // پشتیبانیToolStripMenuItem
+            // 
+            this.پشتیبانیToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.شمارهمیخوایتعارفنکنToolStripMenuItem});
+            this.پشتیبانیToolStripMenuItem.Name = "پشتیبانیToolStripMenuItem";
+            this.پشتیبانیToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.پشتیبانیToolStripMenuItem.Text = "پشتیبانی";
+            // 
+            // شمارهمیخوایتعارفنکنToolStripMenuItem
+            // 
+            this.شمارهمیخوایتعارفنکنToolStripMenuItem.Name = "شمارهمیخوایتعارفنکنToolStripMenuItem";
+            this.شمارهمیخوایتعارفنکنToolStripMenuItem.Size = new System.Drawing.Size(197, 22);
+            this.شمارهمیخوایتعارفنکنToolStripMenuItem.Text = "شماره میخوای تعارف نکن";
+            // 
+            // mnuHelp
+            // 
+            this.mnuHelp.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuHelpExit1});
+            this.mnuHelp.Name = "mnuHelp";
+            this.mnuHelp.Size = new System.Drawing.Size(44, 20);
+            this.mnuHelp.Text = "خروج";
+            // 
+            // mnuHelpExit1
+            // 
+            this.mnuHelpExit1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFileNo});
+            this.mnuHelpExit1.Name = "mnuHelpExit1";
+            this.mnuHelpExit1.Size = new System.Drawing.Size(180, 22);
+            this.mnuHelpExit1.Text = "میخوای خارج شی کلک";
+            // 
+            // mnuFileNo
+            // 
+            this.mnuFileNo.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuHelpExit2});
+            this.mnuFileNo.Name = "mnuFileNo";
+            this.mnuFileNo.Size = new System.Drawing.Size(180, 22);
+            this.mnuFileNo.Text = "نمیزارم";
+            // 
+            // mnuHelpExit2
+            // 
+            this.mnuHelpExit2.Name = "mnuHelpExit2";
+            this.mnuHelpExit2.Size = new System.Drawing.Size(180, 22);
+            this.mnuHelpExit2.Text = "حالا که میخوای برو";
+            this.mnuHelpExit2.Click += new System.EventHandler(this.mnuHelpExit2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Uighur", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(112, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 34);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "به برنامه من خوش اومدی.";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Uighur", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(304, 239);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 31);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "نام کاربری:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Uighur", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(312, 298);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(69, 31);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "رمز عبور:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(112, 245);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 23);
+            this.textBox1.TabIndex = 12;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(112, 304);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(185, 25);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Uighur", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(164, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 42);
+            this.button1.TabIndex = 14;
+            this.button1.Text = "ورود";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 585);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem فایلهاToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem گولخوردیفایلنداریمToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem راهنماToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem دلتخوشهراهنماکجابودToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem پشتیبانیToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem شمارهمیخوایتعارفنکنToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem mnuHelp;
+        private System.Windows.Forms.ToolStripMenuItem mnuHelpExit1;
+        private System.Windows.Forms.ToolStripMenuItem mnuFileNo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ToolStripMenuItem mnuHelpExit2;
+        private System.Windows.Forms.ToolStripMenuItem حالاکهاصرارداریبیاToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+    }
+}
+
---
+++ C#/WindowsForm/Codes project/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+          
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+
+        private void حالاکهاصرارداریبیاToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+
+        private void mnuHelpExit2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("من میدونستم تو دیگه دوستم نداری برو دیگه برنگرد");
+
+        }
+
+        private void حالاکهاصرارداریبیاToolStripMenuItem_Click_1(object sender, EventArgs e)
+        {
+            Form2 form2 = new Form2();
+            form2.ShowDialog();
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("سلام" + " " + textBox1.Text + "به برنامه خوش اومدی");
+        }
+    }
+}
